0e9fb9c681e82583c1931d3c1750ab866d907d67
Vilem-cuda <vilem.cuda.s@gyarab.cz>
2026-06-01 23:38:02 +0200

    pridani trade API endpoints and schemas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,15 @@
-<template>
-  <header>
-    <router-link to="/">Homepage</router-link>
-  </header>
-  <main>
-    <router-view />
-  </main>
-</template>
-
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
+<template>
+    <header>
+        <RouterLink to="/">
+            <h1>Trading Journal</h1>
+        </RouterLink>
+    </header>
+
+    <main>
+        <RouterView />
+    </main>
+</template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import TradeList from '../views/TradeList.vue'
 import TradeDetail from '../views/TradeDetail.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/', name: 'home', component: TradeList},
-        {path: '/trade/:id', name: 'trade/detail', component: TradeDetail}
-
+        {
+            path: '/',
+            name: 'home',
+            component: TradeList
+        },
+        {
+            path: '/trade/:id',
+            name: 'trade-detail',
+            component: TradeDetail
+        }
     ]
 })
+
+export default router
